fd6853908f5816de74c0c0101f298f00b7c6f076
xpanvictor <xpanvictor@gmail.com>
2022-06-24 00:13:55 +0100

    Fix: uncaught caught

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-// websocket, db
+// websocket, db, process errs
 const http = require('http');
 const { PORT } = require('./config/constants').constants;
+const { unhandledRejection } = require('./src/errors/exceptions');
 const App = require('./Main');
 
 const httpServer = http.createServer(App);
 
 const spinServer = async function (server) {
   const main = server.listen(PORT);
+  unhandledRejection(main);
 };
 
 spinServer(httpServer);
