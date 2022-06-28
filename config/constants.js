d10a8d8b9f48565cb76f413fe4f146a96e24649a
xpanvictor <xpanvictor@gmail.com>
2022-06-28 16:20:06 +0100

    Feat: db string

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,8 @@
+const isProduction = require('./stage');
+
 exports.constants = {
   PORT: process.env.PORT || 3000,
+  MONGO_URI: isProduction()
+    ? process.env.MONGO_URI
+    : 'mongodb:localhost/clevermedia',
 };
